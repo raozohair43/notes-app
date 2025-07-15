68bdb86e558b5e3fbc493366cdc3ac9f29d009c4
raozohair43 <raozohair43@gmail.com>
2025-07-15 21:52:37 +0500

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://notes-app-1-ehg5.onrender.com',
   credentials: true
 }));
 app.use(express.json());
