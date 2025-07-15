b4b1fcdd99d11322c531441294dc4682f8960000
raozohair43 <raozohair43@gmail.com>
2025-07-15 17:22:54 +0500

    Update notesService.js

--- frontend/src/services/notesService.js
+++ frontend/src/services/notesService.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/notes';
+const API_URL = 'https://notes-app-7ytd.onrender.com';
 
 export const getNotes = async () => {
   const res = await axios.get(API_URL, { withCredentials: true });
